50ef783afc8cb762d767de51541e43afb915c13b
Rohit <rohitsinghop11@gmail.com>
2026-07-09 14:09:02 +0530

    fix: update base path for new repo name

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Car-rental-website/",
+  base: "/DesiRent-car-rental-application/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
